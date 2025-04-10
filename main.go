ddd082808bfc9c9cda3b613a033419ab6772e873
EhmptyPieczy <laura.f.rodrigues7@aluno.senai.br>
2025-04-10 08:25:09 -0300

    ExerciciosGolang

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
-import ("fmt"
-"strings"
-"sort"
-)
+import ("fmt")
 
 func main(){
-	greeting := "Hello my friends!"
-    fmt.Println(strings.Contains(greeting, "friends"))
-    fmt.Println(strings.ReplaceAll(greeting, 
-        "Hello", "Hi"))
-        fmt.Println(strings.ToUpper(greeting))
-        fmt.Println(strings.Index(greeting,"my"))
-        fmt.Println(strings.Split(greeting, " "))
-        ages := []int {50, 70, 20}
-        sort.Ints(ages)
-        fmt.Println(ages)
-        index := sort.SearchInts(ages, 50)
-        fmt.Println(index)
-        names := []string{"Azul", "Verde", "Vermelho"}
-        sort.Strings(names)
-        fmt.Println(names)
-        fmt.Println(sort.SearchStrings(names, "Azul"))
+	var numeros [5]int
+	var soma int
 
+	fmt.Println("Digite 5 números inteiros:")
+
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Número %d: ", i+1)
+		fmt.Scan(&numeros[i])
+		soma += numeros[i]
+	}
+	fmt.Printf("A soma dos números digitados é: %d\n", soma)
 }
