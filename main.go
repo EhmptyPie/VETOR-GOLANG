01da9fd6c15e9b244868407ce710facb46dd3119
EhmptyPieczy <laura.f.rodrigues7@aluno.senai.br>
2025-04-10 10:40:09 -0300

    ExerciciosGolangIII

--- main.go
+++ main.go
@@ -3,15 +3,37 @@ package main
 import ("fmt")
 
 func main(){
-	var idade int
-	fmt.Println("Quantos anos você tem? ")
-	fmt.Scan(&idade)
-	
-	if idade < 18 {
-		fmt.Println("Você é menor de idade")
-	}else if idade <= 60 {
-	fmt.Println("Você é adulto")
-	}else{
-		fmt.Println("Você é idoso")
+var saldo int 
+var numero int
+var escolha int
+
+fmt.Println("Insira o valor que você gostaria de sacar :")
+fmt.Scan(&saldo)
+
+    fmt.Println("Escolha uma opção:")
+	fmt.Println("1 - Sacar")
+	fmt.Println("2 - Depositar")
+	fmt.Print("Opção: ")
+	fmt.Scan(&escolha)
+
+	if escolha == 1 {
+		fmt.Print("Insira o valor que você gostaria de sacar: ")
+		fmt.Scan(&numero)
+		if numero > saldo {
+			fmt.Println("Saldo insuficiente.")
+		} else {
+			saldo -= numero
+			fmt.Println("Saque realizado com sucesso!")
+		}
+	} else if escolha == 2 {
+		fmt.Print("Insira o valor que você gostaria de depositar: ")
+		fmt.Scan(&numero)
+		saldo += numero
+		fmt.Println("Depósito realizado com sucesso!")
+	} else {
+		fmt.Println("Opção inválida.")
 	}
+
+	fmt.Println("Saldo atual:", saldo)
 }
+	
